a32ddad40cd285005c944b88df2e5da266096d02
curso2 <curso2>
2015-11-26 16:20:14 +0100

    Arreglos de proyecto Spring Security

--- ExpedientesX/src/xfiles/util/DbUtil.java
+++ ExpedientesX/src/xfiles/util/DbUtil.java
@@ -19,22 +19,30 @@ public class DbUtil {
 	}
 
 	public void initialize() {
-		System.out
-				.println("DbUtil.initialize() *************************************** ");
+		System.out.println("DbUtil.initialize() *************************************** ");
 		DataSource dataSource = getDataSource();
 		try {
 			Connection connection = dataSource.getConnection();
 			Statement statement = connection.createStatement();
-			statement.executeUpdate("DROP TABLE USER_AUTHENTICATION IF EXISTS");
-			statement
-					.executeUpdate("CREATE TABLE USER_AUTHENTICATION (USER_ID INTEGER, USERNAME VARCHAR(50), PASSWORD VARCHAR(50), ENABLED BOOLEAN);");
-			statement
-					.executeUpdate("INSERT INTO USER_AUTHENTICATION VALUES(1,'alpha','pass1',TRUE);");
-			statement.executeUpdate("DROP TABLE USER_AUTHORIZATION IF EXISTS");
-			statement
-					.executeUpdate("CREATE TABLE USER_AUTHORIZATION (USER_ROLE_ID INTEGER,USER_ID INTEGER, ROLE VARCHAR(50));");
-			statement
-					.executeUpdate("INSERT INTO USER_AUTHORIZATION VALUES(1,1,'ROLE_ADMIN');");
+			//statement.executeUpdate("DROP TABLE USER_AUTHENTICATION IF EXISTS");
+			statement.executeUpdate(
+					"CREATE TABLE USER_AUTHENTICATION (USER_ID INTEGER, USERNAME VARCHAR(50), PASSWORD VARCHAR(50), ENABLED BOOLEAN)");
+			statement.executeUpdate(
+					"INSERT INTO USER_AUTHENTICATION VALUES(1,'Fernando','94c0a821ece1959c48dbc789bcf024b9',TRUE)");
+			statement.executeUpdate(
+					"INSERT INTO USER_AUTHENTICATION VALUES(2,'Mulder','cd2adcfcfae70a64c5870b7f79ae4945',TRUE)");
+			statement.executeUpdate(
+					"INSERT INTO USER_AUTHENTICATION VALUES(3,'Skinner','829679019772c945c70602096268737c',TRUE)");
+			statement.executeUpdate(
+					"INSERT INTO USER_AUTHENTICATION VALUES(4,'Scully','4fb2b097cb9690d77cf7f2246b782fc3',TRUE)");
+
+			//statement.executeUpdate("DROP TABLE USER_AUTHORIZATION IF EXISTS");
+			statement.executeUpdate(
+					"CREATE TABLE USER_AUTHORIZATION (USER_ROLE_ID INTEGER,USER_ID INTEGER, ROLE VARCHAR(50))");
+			statement.executeUpdate("INSERT INTO USER_AUTHORIZATION VALUES(1,1,'ROLE_AGENT')");
+			statement.executeUpdate("INSERT INTO USER_AUTHORIZATION VALUES(2,2,'ROLE_SPECIAL_AGENT')");
+			statement.executeUpdate("INSERT INTO USER_AUTHORIZATION VALUES(2,4,'ROLE_SPECIAL_AGENT')");
+			statement.executeUpdate("INSERT INTO USER_AUTHORIZATION VALUES(3,3,'ROLE_ASSISTANT_DIRECTOR')");
 
 			statement.close();
 			connection.close();
