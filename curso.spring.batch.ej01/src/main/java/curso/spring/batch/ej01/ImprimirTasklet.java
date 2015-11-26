64d903be27ced1c516d240d03def0715a7ab5f64
curso2 <curso2>
2015-11-26 18:57:12 +0100

    Commit final

--- curso.spring.batch.ej01/src/main/java/curso/spring/batch/ej01/ImprimirTasklet.java
+++ curso.spring.batch.ej01/src/main/java/curso/spring/batch/ej01/ImprimirTasklet.java
@@ -1,5 +1,6 @@
 package curso.spring.batch.ej01;
 
+import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
